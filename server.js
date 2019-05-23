a34b11704ea639aff520ad4ceb5c4af5698604f4
jal9626 <jal9626@gmail.com>
2019-05-22 22:09:25 -0400

    added get and post endpoints

--- server.js
+++ server.js
@@ -4,4 +4,40 @@ const server = express();
 
 // your code here
 
+const db = require('./data/accounts-model.js')
+
+server.use(express.json());
+
+
+server.get('/api/accounts', async (req, res) => {
+    try{
+        const accounts = await db.find(req.query);
+        res.status(200).json(accounts);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'error retrieving the posts'
+        });
+    }
+});
+
+server.post('/api/accounts', async (req, res) => {
+    const newAccount = req.body;
+    if(newAccount){
+        try {
+            const account = await db.add(req.body);
+            res.status(201).json(account);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                message: 'error adding account'
+            });
+        }
+    } else {
+        res.status(400).json({
+            err: 'text property missing'
+        });
+    }   
+});
+
 module.exports = server;
